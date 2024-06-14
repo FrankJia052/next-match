088961365702183814a6ff76ab5d1a121626c0f5
Frank Jia <jurong@claclaws.com>
2024-06-14 11:52:10 -0400

    End of section 2

---
+++ src/app/members/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function layout() {
+  return (
+    <div className='bg-green-600 h-screen'>
+        <h1 className='text-2xl'>app/members/layout.tsx</h1>
+    </div>
+  )
+}
