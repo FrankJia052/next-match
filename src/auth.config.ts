6d8f96d7263c41ed0d1373ce555b3a7824db4a70
Frank Jia <jurong@claclaws.com>
2024-06-17 19:29:49 -0400

    20240617

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,5 +1,27 @@
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
-import type { NextAuthConfig } from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+
+import { CredentialsSignin, type NextAuthConfig } from "next-auth"
+import { loginSchema } from "./lib/schemas/loginSchema"
+import { getUserByEmail } from "./app/actions/authActions"
+import { compare } from "bcryptjs"
  
-export default { providers: [GitHub] } satisfies NextAuthConfig
+export default { providers: [Credentials({
+    name: "credentials",
+    async authorize(creds) {
+       const validated = loginSchema.safeParse(creds);
+
+       if (validated.success) {
+            const {email, password} = validated.data
+
+            const user = await getUserByEmail(email)
+            // check the user & password
+            if(!user || !(await compare(password, user.passwordHash))) return null
+
+            return user
+       }
+
+       return null;
+    }
+})] } satisfies NextAuthConfig
--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,10 +1,13 @@
-import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react'
+import { Button, Navbar, NavbarBrand, NavbarContent } from '@nextui-org/react'
 import Link from 'next/link'
-import React from 'react'
+import React, { Fragment } from 'react'
 import { GiMatchTip } from 'react-icons/gi'
 import NavLink from './NavLink'
+import { auth } from '@/auth'
+import UserMenu from './UserMenu'
 
-export default function TopNav() {
+export default async function TopNav() {
+    const session = await auth()
     return (
         <Navbar
             maxWidth='xl'
@@ -19,20 +22,28 @@ export default function TopNav() {
             }}
         >
             <NavbarBrand as={Link} href="/">
-                <GiMatchTip size={40}/>
+                <GiMatchTip size={40} />
                 <div className='font-bold text-3xl flex'>
                     <span className='text-gray-900'>Next</span>
                     <span className='text-gray-200'>Match</span>
                 </div>
             </NavbarBrand>
             <NavbarContent justify='center' className='text-gray-200'>
-                <NavLink href='/members' label='Matches'/>
-                <NavLink href='/lists' label='Lists'/>
-                <NavLink href='/messages' label='Messages'/>
+                <NavLink href='/members' label='Matches' />
+                <NavLink href='/lists' label='Lists' />
+                <NavLink href='/messages' label='Messages' />
             </NavbarContent>
             <NavbarContent justify='end'>
-                <Button as={Link} href="/login" variant='bordered' className='text-white'>login</Button>
-                <Button variant='bordered' as={Link} href="/register" className='text-white'>register</Button>
+                {
+                    session?.user ? (
+                        <UserMenu user={session.user} />
+                    ) : (
+                        <Fragment>
+                            <Button as={Link} href="/login" variant='bordered' className='text-white'>login</Button>
+                            <Button variant='bordered' as={Link} href="/register" className='text-white'>register</Button>
+                        </Fragment>
+                    )
+                }
             </NavbarContent>
         </Navbar>
     )
