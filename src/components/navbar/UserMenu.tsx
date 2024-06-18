81bcecffea00146bb084058b80849420bfbc88d5
Frank Jia <jurong@claclaws.com>
2024-06-18 18:56:10 -0400

    end of section 3

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { signOut } from '@/auth'
+import { signOutUser } from '@/app/actions/authActions';
 import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger } from '@nextui-org/react'
 import { Session } from 'next-auth'
 import Link from 'next/link'
@@ -9,33 +9,34 @@ type Props = {
     user: Session['user']
 }
 
-export default function UserMenu({user}: Props) {
-  return (
-    <Dropdown placement='bottom-end'>
-        <DropdownTrigger>
-            <Avatar
-                isBordered
-                as='button'
-                className='transition-transform'
-                color='secondary'
-                name={user?.name || 'user avatar'}
-                size='sm'
-                src={user?.image || '/images/user.png'}
-            />
-        </DropdownTrigger>
-        <DropdownMenu variant='flat' aria-label='User actions menu'>
-            <DropdownSection showDivider>
-                <DropdownItem isReadOnly as='span' className='h-14 flex flex-row' aria-label='username'>
-                    Signed in as {user?.name}
-                </DropdownItem>
+export default function UserMenu({ user }: Props) {
+    return (
+        <Dropdown placement='bottom-end'>
+            <DropdownTrigger>
+                <Avatar
+                    isBordered
+                    as='button'
+                    className='transition-transform'
+                    color='secondary'
+                    name={user?.name || 'user avatar'}
+                    size='sm'
+                    src={user?.image || '/images/user.png'}
+                />
+            </DropdownTrigger>
+            <DropdownMenu variant='flat' aria-label='User actions menu'>
+                <DropdownSection showDivider>
+                    <DropdownItem isReadOnly as='span' className='h-14 flex flex-row' aria-label='username'>
+                        Signed in as {user?.name}
+                    </DropdownItem>
+                </DropdownSection>
+
                 <DropdownItem as={Link} href='/members/edit'>
                     Edit profile
                 </DropdownItem>
-                <DropdownItem color='danger' onClick={async () => await signOut()}>
-                    Signout
+                <DropdownItem color='danger' onClick={async () => await signOutUser()}>
+                    Log out
                 </DropdownItem>
-            </DropdownSection>
-        </DropdownMenu>
-    </Dropdown>
-  )
+            </DropdownMenu>
+        </Dropdown>
+    )
 }
