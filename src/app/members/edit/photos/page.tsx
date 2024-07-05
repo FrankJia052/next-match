9109458883e2b4e7e6fce7291a260e6726255b6a
Frank Jia <jurong@claclaws.com>
2024-07-05 17:34:36 -0400

    upload-image-finish

--- src/app/members/edit/photos/page.tsx
+++ src/app/members/edit/photos/page.tsx
@@ -1,10 +1,11 @@
 import { getAuthUserId } from '@/app/actions/authActions'
 import { getMemberPhotosByUserId } from '@/app/actions/memberActions';
 import DeleteButton from '@/components/DeleteButton';
-import ImageUploadButton from '@/components/ImageUploadButton';
 import StarButton from '@/components/StarButton';
 import { CardBody, CardHeader, Divider, Image } from '@nextui-org/react'
 import React from 'react'
+import MemberPhotoUpload from './MemberPhotoUpload';
+import MemberImage from '@/components/MemberImage';
 
 export default async function page() {
     const userId = await getAuthUserId();
@@ -16,18 +17,11 @@ export default async function page() {
             </CardHeader>
             <Divider />
             <CardBody>
-                <div className='pt-5 pl-5'>
-                    <ImageUploadButton/>
-                </div>
+                <MemberPhotoUpload/>
                 <div className='grid grid-cols-5 gap-3 p-5'>
                     {photos && photos.map(photo => (
                         <div key={photo.id} className='relative'>
-                            <Image 
-                                width={220}
-                                height={220}
-                                src={photo.url}
-                                alt='Image of user'
-                            />
+                            <MemberImage photo={photo} />
                             <div className='absolute top-3 left-3 z-50'>
                                 <StarButton selected={false} loading={false}/>
                             </div>
